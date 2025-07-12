83e3bc51700050a1ff26b1016eae0101fb89c9d6
saJeremyQin <343220868@qq.com>
2025-07-12 21:49:33 +0930

    Update docker-deploy.yml and build-push-image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 set -e
 
+# ==== Config ====
 REGION="ap-southeast-2"
 AWS_ACCOUNT_ID=${AWS_ACCOUNT_ID:-$(aws sts get-caller-identity --query Account --output text)}
 ECR_REGISTRY="${AWS_ACCOUNT_ID}.dkr.ecr.$REGION.amazonaws.com"
 ECR_REPOSITORY="emenu/emenu-admin"
+IMAGE_TAG=$(git rev-parse HEAD)  # full commit SHA
 TAG="latest"
 
-# Get the commit SHA（12 digits，the same with GitHub Actions）
-IMAGE_TAG=$(git rev-parse --short=12 HEAD)
-
+# ==== Build ====
 echo "🧱 Building Docker image..."
 docker buildx build --platform linux/amd64 -t "${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}"  .
 
 # latest tag
-docker tag "${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}" "${ECR_REGISTRY}/${ECR_REPOSITORY}:latest"
+docker tag "${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}" "${ECR_REGISTRY}/${ECR_REPOSITORY}:${TAG}"
 
+# ==== Push ====
 echo "🔐 Logging in to ECR..."
 aws ecr get-login-password --region "${REGION}" | docker login --username AWS --password-stdin "$ECR_REGISTRY"
 
@@ -28,4 +29,5 @@ echo "✅ Done! Image pushed:"
 echo "    - ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}"
 echo "    - ${ECR_REGISTRY}/${ECR_REPOSITORY}:${TAG}"
 
-aws ecs update-service --cluster emenu-admin-cluster --service emenu-admin-service --force-new-deployment
+# ==== ECS Redeploy ====
+aws ecs update-service --cluster emenu-admin-cluster --service emenu-admin-service --force-new-deployment  --region "${REGION}"
