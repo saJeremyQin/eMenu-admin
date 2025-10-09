2e717f566852c16a36bd528ba77c0a3438a1de59
saJeremyQin <343220868@qq.com>
2025-10-09 14:18:51 +1030

    remove cognito-identity

--- infra/modules/core/cognito-identity/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "identity_pool_id" {
-  description = "Cognito Identity Pool ID"
-  value       = aws_cognito_identity_pool.main.id
-}
-
-output "authenticated_role_arn" {
-  description = "ARN of the authenticated role"
-  value       = aws_iam_role.authenticated.arn
-}
